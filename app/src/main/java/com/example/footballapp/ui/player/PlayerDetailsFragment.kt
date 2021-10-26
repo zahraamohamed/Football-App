4e16478dd0efa236d3c90d41dc45d3e37b669735
zahraa resen <zaawory98@gmail.com>
2021-10-27 01:12:16 +0300

    create navigation graph

--- app/src/main/java/com/example/footballapp/ui/player/PlayerDetailsFragment.kt
+++ app/src/main/java/com/example/footballapp/ui/player/PlayerDetailsFragment.kt
@@ -35,7 +35,7 @@ class PlayerDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return inflater.inflate(R.layout.fragment_player_details, container, false)
     }
 
     companion object {
