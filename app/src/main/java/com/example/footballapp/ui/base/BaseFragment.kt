59bc23e1efde75ba3d41e21bca1703340015b7fb
MoeKraken <Mohammed27Zalzala11@gmail.com>
2021-10-27 03:24:12 +0300

    add basefragment

--- app/src/main/java/com/example/footballapp/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/footballapp/ui/base/BaseFragment.kt
@@ -1,4 +1,46 @@
 package com.example.footballapp.ui.base
 
-class BaseFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+abstract class BaseFragment<T: ViewDataBinding, view : ViewModel> : Fragment() {
+
+    private lateinit var _binding : ViewDataBinding
+    protected lateinit var viewModel : view
+
+    val binding : T get() = _binding as T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater,
+            getFragment(),
+            container,
+            false
+        )
+        _binding.lifecycleOwner = viewLifecycleOwner
+        viewModel = ViewModelProvider(this).get(getViewModel())
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
+
+    abstract fun setup()
+
+    abstract fun getViewModel(): Class<view>
+
+    abstract fun getFragment(): Int
+
 }
