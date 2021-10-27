a51ea22082131d2d6769f701eb4c69fffe833ff1
AliJalil <Ali.Alkafeel@gmail.com>
2021-10-27 10:37:18 +0300

    add-base-activity

--- app/src/main/java/com/example/footballapp/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/footballapp/ui/base/BaseActivity.kt
@@ -1,4 +1,19 @@
 package com.example.footballapp.ui.base
 
-class BaseActivity {
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<VB : ViewDataBinding> : AppCompatActivity() {
+    abstract val bindingInflater: (LayoutInflater) -> VB
+    private lateinit var _binding: ViewDataBinding
+    var binding: VB?
+        get() = _binding as VB?
+        set(value) = TODO()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingInflater(layoutInflater)
+        setContentView(_binding.root)
+    }
 }
