3b8462e2041076439dea1e18ab8914befce17bb9
Noor ahmad <NourAhmedAbdulSattar41@gmail.com>
2021-10-28 11:52:44 -0700

    after add base viewModel class

--- app/src/main/java/com/example/footballapp/repository/Repository.kt
+++ app/src/main/java/com/example/footballapp/repository/Repository.kt
@@ -1,4 +1,15 @@
 package com.example.footballapp.repository
 
+import com.example.footballapp.model.Status
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
 class Repository {
+    suspend fun <T>getRandomMatch() :Flow<Status<T?>>{
+        return flow {
+           // emit(....)
+        }
+    }
+
+
 }
