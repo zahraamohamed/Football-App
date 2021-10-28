afb2bc963904f17ea3df32c4c965b458106db5d9
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-10-28 12:18:15 +0300

    add-item-match-design

--- app/src/main/java/com/example/footballapp/ui/league/ScorerViewModel.kt
+++ app/src/main/java/com/example/footballapp/ui/league/ScorerViewModel.kt
@@ -3,5 +3,5 @@ package com.example.footballapp.ui.league
 import androidx.lifecycle.ViewModel
 import com.example.footballapp.ui.base.BaseViewModel
 
-class LeagueViewModel : ViewModel() {
+class ScorerViewModel : ViewModel() {
 }
