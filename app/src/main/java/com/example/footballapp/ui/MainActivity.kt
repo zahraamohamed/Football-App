6b33e0dc4b4c8537e76803967ce7637600594ed5
zahraa resen <zaawory98@gmail.com>
2021-10-29 20:18:18 +0300

    add DataBindingUtil to main activity

--- app/src/main/java/com/example/footballapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/footballapp/ui/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.footballapp.ui
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.asLiveData
+import com.example.footballapp.R
 import com.example.footballapp.databinding.ActivityMainBinding
+import com.example.footballapp.model.network.API
+import com.example.footballapp.repository.FootballRepository
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+     }
+        fun <T> T.lazyLog(tag: String = "LAZY_LOG"): T {
+            Log.i(tag, toString())
+            return this
+        }
     }
-
-    fun <T> T.lazyLog(tag: String = "LAZY_LOG"): T {
-        Log.i(tag, toString())
-        return this
-    }
-}
 
