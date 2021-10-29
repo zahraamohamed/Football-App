8dfd8250c6cd9e0b417903590e4a29a446ed712a
zahraa resen <zaawory98@gmail.com>
2021-10-29 14:43:23 +0300

    add fragment container view

--- app/src/main/java/com/example/footballapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/footballapp/ui/home/HomeFragment.kt
@@ -13,8 +13,5 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
         get() = FragmentHomeBinding::inflate
 
-    override fun setup() {
-    }
-
-
+    override fun setup() {}
 }
