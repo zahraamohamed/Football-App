3cb39d963120e05371f72972171adbb1dacf12f5
Rafal A <rafal.akeel99@gmail.com>
2021-10-29 17:26:46 +0300

    add functionality to team details fragment

---
+++ app/src/main/java/com/example/footballapp/ui/teams/TeamDetailsAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.footballapp.ui.teams
+
+import com.example.footballapp.R
+import com.example.footballapp.model.domain.teamDetailsResponse.Squad
+import com.example.footballapp.ui.base.BaseAdapter
+
+class TeamDetailsAdapter(items: List<Squad>, listener:ITeamDetailsInteractionListener):BaseAdapter<Squad>(items, listener) {
+    override val layoutId: Int
+        get() = R.layout.item_team_details
+}
--- app/src/main/java/com/example/footballapp/ui/teams/TeameViewModel.kt
+++ app/src/main/java/com/example/footballapp/ui/teams/TeameViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.footballapp.ui.teams
 
-class TeameViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.example.footballapp.model.State
+import com.example.footballapp.model.domain.teamDetailsResponse.Squad
+import com.example.footballapp.model.domain.teamDetailsResponse.TeamDetailsResponse
+import com.example.footballapp.repository.FootballRepository
+
+class TeameViewModel:ViewModel() , ITeamDetailsInteractionListener {
+
+    private val repository = FootballRepository
+    private val _matchDetails = MutableLiveData<State<TeamDetailsResponse?>>()
+    val matchDetails: LiveData<State<TeamDetailsResponse?>>
+        get() = _matchDetails
+
+    init {
+        getTeamDetails()
+    }
+
+    private fun getTeamDetails() {
+        val teamId = TeamDetailsResponse().id
+        if (teamId != null) {
+            val teamDetails = repository.getSpecificTeamDetails(teamId).asLiveData()
+        }
+
+    }
+
+    override fun onClickTeamPlayer(playerDetails: Squad) {
+
+    }
 }
