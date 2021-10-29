c511ff37247418d47393d3c3271522f9f8f0dee1
Noor ahmad <NourAhmedAbdulSattar41@gmail.com>
2021-10-29 14:25:05 -0700

    add a webViewUrl Type of mutableLiveData in WebSearchViewModel

--- app/src/main/java/com/example/footballapp/ui/webSearch/WebSearchViewModel.kt
+++ app/src/main/java/com/example/footballapp/ui/webSearch/WebSearchViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.footballapp.ui.webSearch
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class WebSearchViewModel:ViewModel() {
+class WebSearchViewModel :ViewModel() {
+
+  val webViewUrl = MutableLiveData("https://www.google.com")
+
+
 }
