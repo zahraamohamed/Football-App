b697a3767bbe67bcc57a61ad5cc065e49a16271b
Noor ahmad <NourAhmedAbdulSattar41@gmail.com>
2021-10-30 11:57:06 -0700

    add code in StandInteractionListener.kt

--- app/src/main/java/com/example/footballapp/ui/league/standing/StandInteractionListener.kt
+++ app/src/main/java/com/example/footballapp/ui/league/standing/StandInteractionListener.kt
@@ -1,4 +1,8 @@
 package com.example.footballapp.ui.league.standing
 
-interface StandInteractionListener {
+import com.example.footballapp.model.domain.teamRankResponse.Standing
+import com.example.footballapp.ui.base.BaseInteractionListener
+
+interface StandInteractionListener : BaseInteractionListener {
+    fun onClickScorer(stand: Standing)
 }
