049ff467fb664ed6a5031433309ffbbe6999948f
Noor ahmad <NourAhmedAbdulSattar41@gmail.com>
2021-10-30 14:26:59 -0700

    add code in LeagueViewModel.kt

--- app/src/main/java/com/example/footballapp/ui/league/LeagueViewModel.kt
+++ app/src/main/java/com/example/footballapp/ui/league/LeagueViewModel.kt
@@ -1,28 +1,24 @@
 package com.example.footballapp.ui.league
 
-import android.view.View
-import android.widget.Toast
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.navigation.Navigation
 import com.example.footballapp.model.domain.scorerRankResponse.Scorer
 import com.example.footballapp.model.domain.specificCompetitionMatchesResponse.Matche
+import com.example.footballapp.model.domain.teamRankResponse.Standing
 import com.example.footballapp.repository.FootballRepository
 import com.example.footballapp.ui.league.matches.MatchInteractionListener
 import com.example.footballapp.ui.league.scorers.ScorerInteractionListener
-import com.example.footballapp.ui.league.scorers.ScorersFragmentDirections
+import com.example.footballapp.ui.league.standing.StandInteractionListener
 
-class LeagueViewModel : ViewModel(), MatchInteractionListener, ScorerInteractionListener {
+class LeagueViewModel : ViewModel(), MatchInteractionListener, ScorerInteractionListener , StandInteractionListener {
 
     val scorer = FootballRepository.getCompetitionScorers(2001).asLiveData()
     val matches = FootballRepository.getSpecificCompetitionMatches(2001).asLiveData()
+    val stand = FootballRepository.getSpecificTeamRank(2001).asLiveData()
 
-    override fun onClickScorer(scorer: Scorer) {
-
-        }
-
-    override fun onClickMatch(match: Matche) {
-        // TODO("Not yet implemented")
-    }
+    override fun onClickScorer(scorer: Scorer) {}
+    override fun onClickMatch(match: Matche) {}
+    override fun onClickStand(stand: Standing) {}
 
 }
