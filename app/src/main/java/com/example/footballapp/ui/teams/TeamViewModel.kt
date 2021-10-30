b90b1658ff1f54760797bb2720a6f52d6689e1ee
zahraa resen <zaawory98@gmail.com>
2021-10-30 19:39:44 +0300

    merge team details fragment to develop and clean the code

--- app/src/main/java/com/example/footballapp/ui/teams/TeamViewModel.kt
+++ app/src/main/java/com/example/footballapp/ui/teams/TeamViewModel.kt
@@ -3,27 +3,13 @@ package com.example.footballapp.ui.teams
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.example.footballapp.model.State
 import com.example.footballapp.model.domain.teamDetailsResponse.Squad
-import com.example.footballapp.model.domain.teamDetailsResponse.TeamDetailsResponse
 import com.example.footballapp.repository.FootballRepository
 
-class TeamViewModel:ViewModel() , ITeamDetailsInteractionListener {
+class TeamViewModel : ViewModel(), ITeamDetailsInteractionListener {
 
+    val teamDetails = FootballRepository.getSpecificTeamDetails(18).asLiveData()
 
-//    private val _matchDetails = MutableLiveData<State<TeamDetailsResponse?>>()
-    val teamDetails=FootballRepository.getSpecificTeamDetails(18).asLiveData()
-
-
-
-//
-//    private fun getTeamDetails() {
-//        val teamId = TeamDetailsResponse().id
-//        if (teamId != null) {
-//            val teamDetails = FootballRepository.getSpecificTeamDetails(teamId).asLiveData()
-//        }
-//
-//    }
 
     override fun onClickTeamPlayer(playerDetails: Squad) {
 
