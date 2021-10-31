03cb88738e046915e6232e1851979bc51f9de8d6
Rafal A <rafal.akeel99@gmail.com>
2021-11-01 02:37:50 +0300

    add player details fragment

--- app/src/main/java/com/example/footballapp/ui/player/PlayerDetailsFragment.kt
+++ app/src/main/java/com/example/footballapp/ui/player/PlayerDetailsFragment.kt
@@ -8,14 +8,10 @@ import com.example.footballapp.databinding.FragmentPlayerDetailsBinding
 import com.example.footballapp.ui.base.BaseFragment
 
 class PlayerDetailsFragment : BaseFragment<FragmentPlayerDetailsBinding>() {
-    private val args: PlayerDetailsFragmentArgs by navArgs()
     override val viewModel: PlayerViewModel by viewModels()
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentPlayerDetailsBinding
         get() = FragmentPlayerDetailsBinding::inflate
 
     override fun setup() {
-//        binding.textView.text = args.playerId.toString()
-//        sharedElementEnterTransition = TransitionInflater.from(context)
-//            .inflateTransition(android.R.transition.move)
     }
 }
--- app/src/main/java/com/example/footballapp/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/example/footballapp/ui/player/PlayerViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.footballapp.ui.player
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.example.footballapp.repository.FootballRepository
 
 class PlayerViewModel : ViewModel() {
+
+    val playerDetails = FootballRepository.getPlayerDetails(44).asLiveData()
 }
