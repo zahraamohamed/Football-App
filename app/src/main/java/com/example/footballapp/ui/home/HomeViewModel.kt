73deaffd06bd74d01f08e1c78517983ccaf30fba
zahraa resen <zaawory98@gmail.com>
2021-11-10 03:10:34 +0300

    edit item live match design

--- app/src/main/java/com/example/footballapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/footballapp/ui/home/HomeViewModel.kt
@@ -1,9 +1,9 @@
 package com.example.footballapp.ui.home
 
 import androidx.lifecycle.*
-import com.example.footballapp.model.domain.footballNewsResponse.Article
 import com.example.footballapp.model.State
 import com.example.footballapp.model.State.*
+import com.example.footballapp.model.domain.footballNewsResponse.Article
 import com.example.footballapp.repository.FootballRepository
 import com.example.footballapp.repository.NewsFootballRepository
 import com.example.footballapp.util.Event
@@ -15,9 +15,30 @@ class HomeViewModel : ViewModel(), HomeInteractionListener {
     val competitions = FootballRepository.filterDataCompetitions().asLiveData()
     val news = NewsFootballRepository.getFootballNews().asLiveData()
 
+
     private val _navigateToLeagueDetails = MutableLiveData<Event<Pair<Int, String>>>()
     val navigateToLeagueDetails: LiveData<Event<Pair<Int, String>>> get() = _navigateToLeagueDetails
     private val clickItemMatch = MutableLiveData<Int?>()
+
+
+
+
+
+    fun onLeagueClicked(leagueId: Int?, leagueName: String? = null) {
+        _navigateToLeagueDetails.value = Event(Pair(leagueId ?: 0, leagueName ?: ""))
+    }
+
+    override fun onClickItemMatch(matchId: Int?) {
+        clickItemMatch.postValue(matchId)
+    }
+
+    private val _navigateToWebView = MutableLiveData<Event<Article>>()
+    val navigateToWebView: LiveData<Event<Article>> get() = _navigateToWebView
+
+    override fun onNewsClick(articleDetails: Article) {
+        _navigateToWebView.postValue(Event(articleDetails))
+    }
+
 
     var statusHome = MediatorLiveData<State<Any?>>().apply {
         addSource(competitions) { postValue(checkStatus(it)) }
@@ -38,21 +59,5 @@ class HomeViewModel : ViewModel(), HomeInteractionListener {
 
             else -> null
         }
-
-
-    fun onLeagueClicked(leagueId: Int?, leagueName: String? = null) {
-        _navigateToLeagueDetails.value = Event(Pair(leagueId ?: 0, leagueName ?: ""))
-    }
-
-    override fun onClickItemMatch(matchId: Int?) {
-        clickItemMatch.postValue(matchId)
-    }
-
-    private  val _navigateToWebView = MutableLiveData <Event<Article>>()
-    val navigateToWebView : LiveData <Event<Article>>  get() = _navigateToWebView
-
-    override fun onNewsClick(articleDetails: Article) {
-        _navigateToWebView.postValue(Event(articleDetails))
-    }
 
 }
